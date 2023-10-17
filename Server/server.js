db92973e770b1328cc9a4659bff46e0cb3214fbc
Veekthor01 <veekthor01@gmail.com>
2023-10-17 20:46:34 +0300

    update server code

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const urlShortenRoute = require('./urlShortenRoute');
+const urlCustomRoute = require('./urlCustomRoute');
+const urlRedirectRoute = require('./urlRedirectRoute');
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +11,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
+app.use('/shorten', urlShortenRoute);
+app.use('/custom', urlCustomRoute);
+app.use('/redirect', urlRedirectRoute);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ error: err.message });
+  });
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on PORT: ${PORT}`);
+    console.log(`Server is listening on PORT ${PORT}`);
 });
