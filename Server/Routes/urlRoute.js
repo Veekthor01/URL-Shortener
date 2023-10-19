f094b6ad9aabdf7abb597afb1572c3502512868a
Veekthor01 <veekthor01@gmail.com>
2023-10-19 22:59:41 +0300

    update route

--- Server/Routes/urlRoute.js
+++ Server/Routes/urlRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../DB/db');
 const validUrl = require('valid-url');
 
 const baseURL = 'http://localhost:3000';
