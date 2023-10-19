d51d0722a798c9d0c9f5b6b857e3655ccc344943
Veekthor01 <veekthor01@gmail.com>
2023-10-19 22:59:23 +0300

    update route

--- Server/Routes/redirectRoute.js
+++ Server/Routes/redirectRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const pool = require('../DB/db');
 
 router.get('/:short_id', async (req, res) => {
     try {
