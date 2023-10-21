503dd1d87e67e4b2c4a73492daa57c3589f58327
Veekthor01 <veekthor01@gmail.com>
2023-10-21 23:18:46 +0300

    update server code

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 const urlRoute = require('./Routes/urlRoute');
 const redirectRoute = require('./Routes/redirectRoute');
 const customRoute = require('./Routes/customRoute');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(morgan('dev'));
 
 app.use('/url', urlRoute);
