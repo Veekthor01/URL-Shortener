0d9eda481b06ff73278c0e33ed5b7c74d5a494e7
Veekthor01 <veekthor01@gmail.com>
2023-10-22 21:43:44 +0300

    log to server

--- Server/DB/db.js
+++ Server/DB/db.js
@@ -7,6 +7,7 @@ const database = process.env.DB_DATABASE;
 const password = process.env.DB_PASSWORD;
 const port = process.env.DB_PORT;
 
+// Create a new connection pool
 const pool = new Pool({
     user: user,
     host: host,
@@ -14,5 +15,16 @@ const pool = new Pool({
     password: password,
     port: port,
 });
+
+// Log successful connection
+pool.on('connect', () => {
+    console.log('Database pool connected');
+});
+
+// Handle connection errors
+pool.on('error', (err, client) => {
+    console.error('Database pool error:', err);
+    process.exit(-1);
+  });
 
 module.exports = pool;
