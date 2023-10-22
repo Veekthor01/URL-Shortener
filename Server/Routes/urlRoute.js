8f752f568893f4d7a6abbfc796bf537ff7b53973
Veekthor01 <veekthor01@gmail.com>
2023-10-22 17:45:35 +0300

    update url routes

--- Server/Routes/urlRoute.js
+++ Server/Routes/urlRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../DB/db');
 const validUrl = require('valid-url');
 
-const baseURL = 'http://localhost:3000';
+const baseURL = 'http://localhost:5000';
 
 router.post('/', async (req, res) => {
     try {
@@ -11,18 +11,20 @@ router.post('/', async (req, res) => {
         const { nanoid } = await import('nanoid'); // Import nanoid dynamically because it is an ESM module
         let short_url;
         let short_id = nanoid(8);
-        // Check if the base URL is valid
-        /*if (validUrl.isUri(baseURL)) {
-            console.log('Valid baseUrl');
-          } else {
-            console.error('Invalid baseUrl');
-          } */
         // Check if the long URL is valid
         if (validUrl.isUri(long_url)) {
             console.log('Valid long URL');
           } else {
             res.status(400).json({ error: 'Invalid long URL' });
-          }
+          };
+          // Check if the long URL exceeds 2000 characters
+        if (long_url.length > 2000) {
+            return res.status(400).json({ error: 'URL cannot exceed 2000 characters' });
+        };
+        // Check if the custom URL exceeds 100 characters
+        if (custom_url && custom_url.length > 200) {
+            return res.status(400).json({ error: 'Custom URL cannot exceed 200 characters' });
+        };
         // Check if the custom URL is already in use
         if (custom_url) {
             const existingUrl = await pool.query(
@@ -33,11 +35,11 @@ router.post('/', async (req, res) => {
                 return res.status(400).json({ error: 'Custom URL already in use' });
             }
             // Use the custom URL as the short URL
-            short_url = `${baseURL}/custom/${custom_url}`;
+            short_url = `${baseURL}/:/${custom_url}`;
         } else {
             // Generate a short URL using nanoid
             short_url = `${baseURL}/${short_id}`;
-        }
+        };
         // Insert the URL into the database
         const newUrl = await pool.query(
             `INSERT INTO url (long_url, short_url, custom_url, short_id) VALUES ($1, $2, $3, $4) RETURNING short_url`,
