464b0e84f81d68fb829eed47ec9fcb7535255969
Veekthor01 <veekthor01@gmail.com>
2023-10-24 19:28:00 +0300

    refactor code

--- Server/Routes/customRoute.js
+++ Server/Routes/customRoute.js
@@ -4,11 +4,14 @@ const pool = require('../DB/db');
 
 router.get('/:/:custom_url', async (req, res) => {
     try {
+        // Get the custom URL from the request parameters
         const { custom_url } = req.params;
+        // Get the long URL from the database using the custom URL
         const result = await pool.query(
             `SELECT long_url FROM url WHERE custom_url = $1`,
             [custom_url]
         );
+        // If the long URL is found, redirect to it
         if (result.rows.length > 0) {
             return res.redirect(result.rows[0].long_url);
         } else {
