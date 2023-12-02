fe7f1a5ffdab47b2fcd3fd6eb464f49263d130d0
Veekthor01 <veekthor01@gmail.com>
2023-12-02 21:52:03 +0300

    Update CORS configuration

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(morgan('dev'));
 
 // Use your database connection in routes and controllers
