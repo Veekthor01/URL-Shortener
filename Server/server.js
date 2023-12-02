76536793796b35b5e8045c24a403c3422eeceeb1
Veekthor01 <veekthor01@gmail.com>
2023-12-02 22:06:43 +0300

    Add CORS configuration

--- Server/server.js
+++ Server/server.js
@@ -9,9 +9,14 @@ const customRoute = require('./Routes/customRoute');
 
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: '*' }));
 app.use(morgan('dev'));
 
 // Use your database connection in routes and controllers
